1d4b2af74e31c85545c58377bc474f09e13a62ea
DBREngineer <t.streiber@googlemail.com>
2018-05-27 22:54:07 +0200

    ...

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ apt-get install git net-tools openvpn easy-rsa
 git clone 'https://github.com/DBREngineer/VPNServer.git'
 
 mkdir /etc/openvpn/easy-rsa/ && cp -R /usr/share/easy-rsa/ /etc/openvpn/
+mkdir /root/vpn_config
+
